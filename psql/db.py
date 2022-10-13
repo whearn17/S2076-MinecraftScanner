f8b214e4e620733a3f147107adce8ad4bb8050e3
William Hearn <williamhearn17@gmail.com>
2022-10-13 15:47:52 -0400

    - Add reset_table for clearing and making new minecraft table - Remove check_pass as reset_table tests the connection for you

--- psql/db.py
+++ psql/db.py
@@ -2,12 +2,12 @@ import psycopg2
 
 
 def send(serverlist, host, name, user, password):
-    conn = psycopg2.connect(host=host, dbname=name,
-                            user=user, password=password,
-                            sslmode="require")
-    cur = conn.cursor()
-    for server in serverlist:
-        try:
+    try:
+        conn = psycopg2.connect(host=host, dbname=name,
+                                user=user, password=password,
+                                sslmode="require")
+        cur = conn.cursor()
+        for server in serverlist:
             cur.execute(f"INSERT INTO minecraft"
                         f"(ip_address, version, protocol, "
                         f"description, p_online, p_max) "
@@ -15,17 +15,34 @@ def send(serverlist, host, name, user, password):
                         (server.ip, server.version, server.prot,
                          server.description, server.p_online,
                          server.p_max))
-        except Exception as e:
-            f = open("log.txt", "a")
-            f.write(f"{e}\n\n")
-            f.close()
+    except Exception as e:
+        f = open("log.txt", "a")
+        f.write(f"{e}\n\n")
+        f.close()
     cur.close()
     conn.commit()
     conn.close()
 
 
-def check_pass(host, name, user, password):
-    conn = psycopg2.connect(host=host, dbname=name,
-                            user=user, password=password,
-                            sslmode="require")
+# Clear the old minecraft table and reset
+def reset_table(host, name, user, password):
+    try:
+        conn = psycopg2.connect(host=host, dbname=name,
+                                user=user, password=password,
+                                sslmode="require")
+        cur = conn.cursor()
+        cur.execute("DROP TABLE minecraft;")
+        cur.execute("CREATE TABLE IF NOT EXISTS minecraft"
+                    "(ip_address inet PRIMARY KEY,"
+                    "version VARCHAR (800),"
+                    "protocol VARCHAR (500),"
+                    "description VARCHAR (5000),"
+                    "p_online int,"
+                    "p_max int);")
+    except Exception as e:
+        f = open("log.txt", "a")
+        f.write(f"{e}\n\n")
+        f.close()
+    cur.close()
+    conn.commit()
     conn.close()

b1675ab92b37ab6e8335d86cacc25a08374b44dc
William Hearn <williamhearn17@gmail.com>
2022-10-15 10:53:13 -0400

    Minor Changes - add the option for custom table names and ssl mode - properly handle variables in try except statements - add except for when table doesn't exist and create it

--- psql/db.py
+++ psql/db.py
@@ -1,49 +1,65 @@
 import psycopg2
+import psycopg2.errors
+import traceback
 
 
 # Send the servers to the database
-def send(serverlist, host, name, user, password):
+def send(server, host, name, user, password, table, ssl, ip):
     try:
         conn = psycopg2.connect(host=host, dbname=name,
                                 user=user, password=password,
-                                sslmode="require")
+                                sslmode=ssl)
         cur = conn.cursor()
-        for server in serverlist:
-            cur.execute(f"INSERT INTO minecraft"
-                        f"(ip_address, version, protocol, "
-                        f"description, p_online, p_max) "
-                        f"VALUES(%s, %s, %s, %s, %s, %s);",
-                        (server.ip, server.version, server.prot,
-                         server.description, server.p_online,
-                         server.p_max))
+        cur.execute(f"INSERT INTO {table}"
+                    f"(ip_address, version, protocol, "
+                    f"description, p_online, p_max) "
+                    f"VALUES(%s, %s, %s, %s, %s, %s);",
+                    (ip, server.version.name, server.version.protocol,
+                     server.description, server.players.online,
+                     server.players.max))
+        cur.close()
+        conn.commit()
+        conn.close()
     except Exception as e:
         f = open("log.txt", "a")
-        f.write(f"{e}\n\n")
+        f.write(f"{e}\n\n{traceback.format_exc()}\n\n")
         f.close()
-    cur.close()
-    conn.commit()
-    conn.close()
 
 
 # Clear the old minecraft table and reset
-def reset_table(host, name, user, password):
+def reset_table(host, name, user, password, table, ssl):
     try:
         conn = psycopg2.connect(host=host, dbname=name,
                                 user=user, password=password,
-                                sslmode="require")
+                                sslmode=ssl)
         cur = conn.cursor()
-        cur.execute("DROP TABLE minecraft;")
-        cur.execute("CREATE TABLE IF NOT EXISTS minecraft"
+        cur.execute(f"DROP TABLE {table};")
+        cur.execute(f"CREATE TABLE {table}"
                     "(ip_address inet PRIMARY KEY,"
                     "version VARCHAR (800),"
                     "protocol VARCHAR (500),"
                     "description VARCHAR (5000),"
                     "p_online int,"
                     "p_max int);")
+        cur.close()
+        conn.commit()
+        conn.close()
+    except psycopg2.errors.UndefinedTable:
+        conn = psycopg2.connect(host=host, dbname=name,
+                                user=user, password=password,
+                                sslmode=ssl)
+        cur = conn.cursor()
+        cur.execute(f"CREATE TABLE {table}"
+                    "(ip_address inet PRIMARY KEY,"
+                    "version VARCHAR (800),"
+                    "protocol VARCHAR (500),"
+                    "description VARCHAR (5000),"
+                    "p_online int,"
+                    "p_max int);")
+        cur.close()
+        conn.commit()
+        conn.close()
     except Exception as e:
         f = open("log.txt", "a")
-        f.write(f"{e}\n\n")
+        f.write(f"{e}\n\n{traceback.format_exc()}\n\n")
         f.close()
-    cur.close()
-    conn.commit()
-    conn.close()
